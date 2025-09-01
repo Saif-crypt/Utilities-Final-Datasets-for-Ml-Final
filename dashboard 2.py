740ef887068e11e7d1122ad80e65eb10066754ae
Saif Siddiqui <siddiquisaif728@gmail.com>
2025-09-01 16:48:06 +0530

    Update dashboard 2.py

--- dashboard 2.py
+++ dashboard 2.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 # ===============================
 st.set_page_config(page_title="Utilities Dashboard", layout="wide")
 
-df = pd.read_csv("utilities_3compressors_final.csv")
+df = pd.read_csv("utilities_final.csv")
 df["Timestamp"] = pd.to_datetime(df["Timestamp"])
 
 # Sidebar
